98daf94167826b66e5c59b0b25426bf07c587eb4
Nitish Kumar Singh <nitishk72@gmail.com>
2021-06-15 11:05:50 +0530

    relating html and react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="wrapper">
       <h1>This is heading 1</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus soluta, ex dolorem accusantium, in veniam laborum
+        qui ratione consequatur, doloremque minima explicabo optio! Asperiores quos hic numquam, cum id doloribus!</p>
+
+        <form action="./submit/">
+          <input placeholder="email" type="email"/>
+          <br/>
+          <button type="submit">Submit</button>
+        </form>
     </div>
   );
 }
