6f1b4d4b281e5cf4c9e0674f62861643d75b04bf
Nitish Kumar Singh <nitishk72@gmail.com>
2021-06-21 11:52:03 +0530

    getting data from input field

--- src/page/contact.js
+++ src/page/contact.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import { Container, Card, Button, Form } from 'react-bootstrap';
 
 
 function Contact(props) {
+  const name = createRef();
+  const reason = createRef();
+  const message = createRef();
+
+  const submitForm = () => {
+    const name_value = name.current.value;
+    const reason_value = reason.current.value;
+    const message_value = message.current.value;
+    const val = {
+      name: name_value,
+      reason: reason_value,
+      message: message_value,
+    };
+    console.log(val);
+  }
+
   return (
     <Container>
       <h1>Contact</h1>
@@ -17,14 +33,14 @@ function Contact(props) {
 
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Full Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter name" />
+              <Form.Control type="text" ref={name} placeholder="Enter name" />
               <Form.Text className="text-muted">
                 Enter Your Full Name
               </Form.Text>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="reason">
-              <Form.Control as="select">
+              <Form.Control as="select" ref={reason}>
                 <option value="none">Reason to Contact</option>
                 <option value="feedback">Feeback</option>
                 <option value="issue">Issue</option>
@@ -34,10 +50,10 @@ function Contact(props) {
 
             <Form.Group className="mb-3" controlId="message">
               <Form.Label>Your Message</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Control as="textarea" rows={3} ref={message} />
             </Form.Group>
 
-            <Button variant="primary" type="submit" className="btn-block">
+            <Button onClick={submitForm} variant="primary" type="button" className="btn-block">
               Submit
             </Button>
           </Form>
